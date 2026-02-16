9c46255bdeb19460ecf6bac4df39f3431dad6822
Kykylllka <ivan.malanych@mail.ru>
2026-02-16 23:51:31 +0500

    added html-plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vite';
+import injectHTML from 'vite-plugin-html-inject';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   server: {
     port: 3000,
     open: true,
   },
+  plugins: [
+    injectHTML(),
+    ViteImageOptimizer({
+      /* pass your config */
+      png: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+    }),
+  ],
 });

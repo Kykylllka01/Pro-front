da042364df420820373911360a3bdc57741dfe27
Kykylllka <ivan.malanych@mail.ru>
2026-02-17 11:56:15 +0500

    Added some plugins

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
 import { defineConfig } from 'vite';
 import injectHTML from 'vite-plugin-html-inject';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 export default defineConfig({
   server: {
     port: 3000,
     open: true,
   },
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
   plugins: [
     injectHTML(),
+    ViteMinifyPlugin(),
     ViteImageOptimizer({
       /* pass your config */
       png: {
